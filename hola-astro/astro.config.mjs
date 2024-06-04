c43d8cda8e8a3e416e2ac9e80cce8b69a6d873ee
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-06-04 10:41:00 +0200

    feat: dynamic routing

--- hola-astro/astro.config.mjs
+++ hola-astro/astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // https://docs.astro.build/en/reference/configuration-reference/#output
+  output: 'server'
+});
